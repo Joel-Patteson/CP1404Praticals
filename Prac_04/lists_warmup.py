4e6bcbff352aa5212ddeb4c99d5d1cf593020061
Joel-Patteson <69024767+Joel-Patteson@users.noreply.github.com>
2020-08-27 13:18:03 +1000

    Update lists_warmup.py

--- Prac_04/lists_warmup.py
+++ Prac_04/lists_warmup.py
@@ -1,12 +1,12 @@
 numbers = [3, 1, 4, 1, 5, 9, 2]
 
-numbers[0] # takes the first element
-numbers[-1] # takes the last element
-numbers[:-1] # displays all elements
-numbers[3:4] # displays 3 and 4 which is 1,5
-5 in numbers # check if 5 is in the numbers
-7 in numbers # check if 7 is in the numbers
-"3" in numbers # check to see if 3 is in the string
+numbers[0] # takes the first element - 3
+numbers[-1] # takes the last element - 2
+numbers[:-1] # displays all elements - 3 1 4 1 5 9 2
+numbers[3:4] # displays 3 and 4 - 1 5
+5 in numbers # check if 5 is in the numbers - true
+7 in numbers # check if 7 is in the numbers - false
+"3" in numbers # check to see if 3 is in the string - false
 numbers + [6, 5, 3] # adds 6, 5 and 3 to the end of the number
 
 
