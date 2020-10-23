85296d11b5dd01f2e878e6138bdb32c9909d8c4e
Joel-Patteson <69024767+Joel-Patteson@users.noreply.github.com>
2020-10-23 10:26:29 +1000

    Update

--- Prac_09/cleanup_files.py
+++ Prac_09/cleanup_files.py
@@ -2,31 +2,32 @@ import os
 
 
 def main():
-    os.chdir('Lyrics')
-    for directory_name, subdirectories, filenames in os.walk('.'):
-        for filename in filenames:
-            new_name = get_fixed_filename(filename)
-            print("Renaming {} to {}".format(filename, new_name))
-
-            full_name = os.path.join(directory_name, filename)
-            new_name = os.path.join(directory_name, new_name)
-            os.rename(full_name, new_name)
-
-
-def get_fixed_filename(filename):
-    new_name = filename.replace("", "_").replace(".TXT", ".txt").replace(filename, filename.title()) \
-        .replace(".Txt", ".txt")
-    print(new_name)
-    return new_name
-
-
-def demo_walk():
+    """Process all subdirectories using os.walk()."""
     os.chdir('Lyrics')
     for directory_name, subdirectories, filenames in os.walk('.'):
         print("Directory:", directory_name)
         print("\tcontains subdirectories:", subdirectories)
         print("\tand files:", filenames)
         print("(Current working directory is: {})".format(os.getcwd()))
+        for name in filenames:
+            old_name = os.path.join(directory_name, name)
+            new_name = os.path.join(directory_name, get_fixed_filename(name))
+            print(new_name)
+            os.rename(old_name, new_name)
+
+
+def get_fixed_filename(filename):
+    """Return a 'fixed' version of filename."""
+    new_name = filename.replace(".TXT", ".txt")
+    new_text = ''
+    for i, letter in enumerate(new_name):
+        if letter.isupper() and not new_name[i - 1] == "(" and not new_name[i - 1] == " " \
+                and not new_name[i - 1] == "_":
+            new_text += ' '
+        new_text += letter
+    new_text = new_text[:new_text.find(".")].title().strip() + new_text[new_text.find("."):].strip()
+    new_text = new_text.replace(" ", "_")
+    return new_text
 
 
 main()
