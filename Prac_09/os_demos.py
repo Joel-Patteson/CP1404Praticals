fbeb8cbda1632b2d2a5dd343c9b3e97a8008c926
Joel-Patteson <69024767+Joel-Patteson@users.noreply.github.com>
2020-10-23 10:25:10 +1000

    update

--- Prac_09/os_demos.py
+++ Prac_09/os_demos.py
@@ -1,17 +1,27 @@
+import shutil
 import os
 
 
 def main():
+    """Demo os module functions."""
     print("Starting directory is: {}".format(os.getcwd()))
+
+    # Change to desired directory
     os.chdir('Lyrics/Christmas')
+
+    # Print a list of all files in current directory
     print("Files in {}:\n{}\n".format(os.getcwd(), os.listdir('.')))
 
+    # Make a new directory
+    # The next time you run this, it will crash if the directory exists
     try:
         os.mkdir('temp')
     except FileExistsError:
         pass
 
+    # Loop through each file in the (current) directory
     for filename in os.listdir('.'):
+        # Ignore directories, just process files
         if os.path.isdir(filename):
             continue
 
@@ -19,24 +29,53 @@ def main():
         print("Renaming {} to {}".format(filename, new_name))
 
 
+        # Option 1: rename file to new name - in place
+        # os.rename(filename, new_name)
+
+        # Option 2: move file to new place, with new name
+        shutil.move(filename, 'temp/' + new_name)
+
+
 def get_fixed_filename(filename):
-    new_name = filename.replace(" ", "_").replace(".TXT", ".txt")
+    """Return a 'fixed' version of filename."""
+    number = 0
+    file_name = filename.replace(".TXT", ".txt")
+    new_name = ""
+    if " " in file_name:
+        for char in file_name:
+            if number == 0:
+                new_name += char.upper()
+                number += 1
+            elif char == " ":
+                new_name += "_"
+            # elif char.isupper() is True:
+            #     new_name += "_"+char
+            else:
+                new_name += char
+    if " " not in file_name:
+        for char in file_name:
+            if number == 0:
+                new_name += char.upper()
+                number += 1
+            # elif char == " ":
+            #     new_name += "_"
+            elif char.isupper() is True:
+                new_name += "_"+char
+            else:
+                new_name += char
     return new_name
 
 
 def demo_walk():
-    os.chdir('Lyrics')
+    """Process all subdirectories using os.walk()."""
+    # os.chdir('Lyrics')
     for directory_name, subdirectories, filenames in os.walk('.'):
         print("Directory:", directory_name)
         print("\tcontains subdirectories:", subdirectories)
         print("\tand files:", filenames)
         print("(Current working directory is: {})".format(os.getcwd()))
 
-        for file in filenames:
-            full_name = os.path.join(directory_name, file)
-            print(os.path.join(directory_name, file))
-            new_name = os.path.join(directory_name, get_fixed_filename(file))
-            os.rename(full_name, new_name)
 
 
+main()
 demo_walk()
